16609e060b9ed62837fd83496319aeefeda2577c
leong34 <51914391+leong34@users.noreply.github.com>
2021-06-21 09:56:07 +0800

    edit function

--- popup/guard.js
+++ popup/guard.js
@@ -29,7 +29,6 @@ if(search !== null){
     search.addEventListener("input", onSearch);
 }
 
-
 function add(){
     arr.push(new Item(
                 document.getElementById('url').value,
@@ -51,8 +50,6 @@ function onError(error){
 }
 
 function readFromStore(){
-    if( document.getElementById("guard_list") === null)
-    return;
     console.log("reading from store");
     var itemList = browser.storage.local.get('items').then(function(result){
         if(result.items === undefined){
@@ -63,14 +60,11 @@ function readFromStore(){
         result.items.forEach(element => {
             arr.push(new Item(element['domain'], element['name'], element['username'], element['password'], element['key']));
             map.set(element['key'], element);
-            showItem(element);
+            if( document.getElementById("guard_list") !== null)
+                showItem(element);
         });
     }, onError);
 }
-
-// function edit(){
-
-// }
 
 function del(key){
     console.log("delete" + key);
@@ -120,7 +114,7 @@ function showItem(obj){
                                 <div class="col-2 d-flex align-items-center justify-content-center">
                                     <img src="http://${domain}/favicon.ico" style="max-width: 35px; max-height: 35px;">
                                 </div>
-                                <div class="col-6">
+                                <div id="edit${obj.key}" class="col-6">
                                     <div class="row">
                                         <div class="col">${obj.name}</div>
                                     </div>
@@ -143,7 +137,7 @@ function showItem(obj){
                                 <div class="col-2 d-flex align-items-center justify-content-center">
                                     <i class="fas fa-globe-americas" style="max-width: 35px; max-height: 35px;"></i>
                                 </div>
-                                <div class="col-6">
+                                <div id="edit${obj.key}" class="col-6">
                                     <div class="row">
                                         <div class="col">${obj.name}</div>
                                     </div>
@@ -164,6 +158,7 @@ function showItem(obj){
     let delid = 'delbtn' + obj.key;
     let cpid = 'cp' + obj.key;
     let cpUsername = 'cpUser' + obj.key;
+    let editKey = 'edit' + obj.key;
 
     document.getElementById(delid).addEventListener('click', function(){
         var confirmation = confirm("Are you sure to delete this?");
@@ -172,7 +167,7 @@ function showItem(obj){
     });
 
     //edit on click listener
-    document.getElementById(obj.key).addEventListener('click', function(){console.log("clicked")});
+    document.getElementById(editKey).addEventListener('click', function(){enterEditPage(obj);});
 
     document.getElementById(cpid).addEventListener('click', function(){clipboardPassword(obj.key);});
     document.getElementById(cpUsername).addEventListener('click', function(){clipboardUsername(obj.key);});
@@ -193,4 +188,112 @@ function onSearch(){
         });
         
     }
+}
+
+function enterEditPage(obj){
+	var documentData = ` <!DOCTYPE html>
+						<html style="scrollbar-width: none;">
+							<head>
+								<meta charset="utf-8">
+								<link rel="stylesheet" href="guard.css"/>
+								<link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
+								<link rel="stylesheet" href="fontawesome/css/all.css"/>
+								<script src="bootstrap/js/bootstrap.js"></script>
+								<style>
+									a {
+										color: inherit;
+									}
+									.fas {
+										color: rgb(133, 135, 136);
+									}
+									.fas:hover {
+										color: rgb(43, 43, 43);
+									}
+									.input-icons i {
+										position: absolute;
+										margin-left: 10px;
+										align-self: center;
+									}
+									  
+									.input-icons {
+										width: 100%;
+										display: flex;
+										margin-right: 10px;
+									}
+									  
+									.icon {
+										padding: 10px;
+										min-width: 40px;
+									}
+									  
+									.input-field {
+										width: 100%;
+										padding: 10px;
+										padding-left: 30px;
+									}
+									#url {
+										background-color: #fff;
+									}
+									.form-group {
+										margin-bottom: 10px;
+									}
+								</style>
+							</head>
+							<body class="p-3" style="overflow-x: hidden; background-color: #ebebeb;">
+								<div class="container position-relative d-flex justify-content-center align-items-center" style="min-width: 480px; min-height: 550px; width: 100%">
+									<form action="" style="min-width: 400px;">
+										<div class="form-group">
+											<label for="url">Active URL</label>
+											<input type="text" value="${obj.domain}" class="form-control" id="url" aria-describedby="url" readonly>
+										</div>
+										<div class="form-group">
+										  <label for="label">Label</label>
+										  <input type="text" autocomplete="off" value="${obj.name}" class="form-control" id="label" aria-describedby="label" placeholder="Enter label">
+										</div>
+										<div class="form-group">
+											<label for="username">Username</label>
+											<input type="text" autocomplete="off" value="${obj.username}" class="form-control" id="username" aria-describedby="username" placeholder="Enter username">
+										  </div>
+										<div class="form-group">
+										  <label for="password">Password</label>
+										  <input type="password" autocomplete="off" value="${obj.password}" class="form-control" id="password" placeholder="Password">
+										</div>
+										
+										<div class="row">
+											<div class="col"><button id="cancel" type="cancel" class="btn btn-warning" style="margin-top: 20px;">CANCEL</button></div>
+											<div class="col d-flex justify-content-end"><button id="updateBtn" type="button" class="btn btn-success" style="margin-top: 20px;" disabled>UPDATE</button></div>
+										</div>
+									</form>
+								</div>
+                                <div id="successText" class="alert alert-success fixed-bottom" style="display: none; margin: 15px;">Success Update</div>
+							</body>
+						</html>`;
+						
+	var newHtml = document.open("updatedForm/html");
+	newHtml.write(documentData);
+	newHtml.close();
+    setTimeout(function(){
+        document.getElementById("updateBtn").disabled = false;
+        document.getElementById("updateBtn").addEventListener('click', function(){
+            edit(obj.key);
+            console.log("UPDATED!!!!");
+        });
+    }, 1000);
+}
+
+function edit(key){
+	var temp = new Item(
+                document.getElementById('url').value,
+                document.getElementById('label').value, 
+                document.getElementById('username').value, 
+                document.getElementById('password').value);
+				
+	map.set(key, temp);
+	arr = []; //Empty the array
+    map.forEach(element => { //Re-add all the data from the hashmap into the array
+        arr.push(element);
+    });
+    browser.storage.local.clear().then(function(){browser.storage.local.set({items: arr});}, onError); //Clear and re-add latest array data into local storage
+    document.getElementById('successText').setAttribute("style", "display: block; margin: 15px;");
+    setTimeout(function(){document.getElementById('successText').setAttribute("style", "display: none");}, 3000);
 }
